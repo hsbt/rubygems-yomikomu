c7ee9628fb74602492a217cf6954bbb99c3d9c52
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2015-12-18 14:43:07 +0900

    rubygems-yomikomu

---
+++ lib/rubygems/yomikomu.rb
@@ -0,0 +1,7 @@
+require "rubygems/yomikomu/version"
+
+module Rubygems
+  module Yomikomu
+    # Your code goes here...
+  end
+end
---
+++ lib/rubygems/yomikomu/version.rb
@@ -0,0 +1,5 @@
+module Rubygems
+  module Yomikomu
+    VERSION = "0.1.0"
+  end
+end
