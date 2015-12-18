faa9cfcdf8111492161f4975afc4735b2a39a55a
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2015-12-18 15:17:40 +0900

    it works

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -1,8 +1,11 @@
 require "rubygems/installer"
+require 'yomikomu'
 
 Gem.post_install do |installer|
-  installer.spec.files.each do |f|
-    next unless f =~ /.rb\z/
-    ::Yomikomu.compile_and_store_iseq(f)
+  installer.spec.full_require_paths.each do |path|
+    Dir.glob(path + "/**/*.rb").each do |f|
+      next unless f =~ /.rb\z/
+      ::Yomikomu.compile_and_store_iseq(f)
+    end
   end
 end
